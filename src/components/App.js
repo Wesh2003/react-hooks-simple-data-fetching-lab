c837fda6d36b60c15a86557488c68c5cac6b4106
Tumaini Muriithi <wendomuriithi@gmail.com>
2023-11-14 14:03:51 +0300

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,24 @@
 // create your App component here
+import { useEffect } from 'react'
+import { useState } from 'react'
+import React from 'react'
+
+
+
+function App() {
+
+
+    const [dog, setDog] = useState('')
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+          .then(r => r.json())
+          .then(data => setDog(data))
+       }, [])
+
+    return (
+        <img src = {dog} alt = "A random dog"/>
+
+    )
+}
+
+export default App;
